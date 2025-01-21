e78259b2049bd61c5ef40084c88fa4c525144af5
Josec0Loc0 <jose.perez.tapiaudla.edu.ec>
2025-01-21 10:25:20 -0500

    Modificaci[on del modelo

--- PerezEvaluacionProgreso/Models/PeliculaJP.cs
+++ PerezEvaluacionProgreso/Models/PeliculaJP.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PerezEvaluacionProgreso.Models
 {
-    public class PeliculaJP 
+    public class PeliculaJP
     {
-        public string Nombre {  get; set; }
-        public string Genero { get; set; }
-        public string Director { get; set; }
-        public string Year { get; set; }
-        public string PosterUrl { get; set; }
+        [JsonProperty("title")]
+        public string Nombre { get; set; }
 
+        [JsonProperty("year")]
+        public string Year { get; set; }
+
+        [JsonProperty("genre")]
+        public List<string> Genero { get; set; }
+
+        [JsonProperty("rating")]
+        public double Rating { get; set; }
+
+        [JsonProperty("director")]
+        public string Director { get; set; }
+
+        [JsonProperty("actors")]
+        public List<string> Actores { get; set; }
+
+        [JsonProperty("plot")]
+        public string Sinopsis { get; set; }
+
+        [JsonProperty("poster")]
+        public string PosterUrl { get; set; }
     }
 }
